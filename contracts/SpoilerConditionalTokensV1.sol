0fd8984188defe56b8817472a49dc068c0b35e12
soobokjin <thdnthdn24@gmail.com>
2024-04-17 15:28:18 +0900

    Implement happypath

--- contracts/SpoilerConditionalTokensV1.sol
+++ contracts/SpoilerConditionalTokensV1.sol
@@ -8,19 +8,22 @@ import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.so
 import {CTHelpers} from "./CTHelpers.sol";
 
 
-// TODO: owner restriction
 // TODO: devide collateral treasury and this logic
+// TODO: owner
+// TODO: restriction
+// TODO: set eventlog
 
 // oracle, questionId 를 조합하여 condition Id 생성. (slot count 는 상관없음)
 // position Id: condition Id + position index 
 struct Condition {
+  bool isInitialized;
   address collateralToken;
   address oracle;
   uint8 positionCount;
   uint256 startBlockNumber;
   uint256 endBlockNumber;
   // key: position id, value: total supply condition open 까지만 업데이트
-  mapping(bytes32 => uint256) positionTotalSupply;
+  mapping(uint256 => uint256) positionTotalSupply;
   // default: max uint8
   uint8 selectedIndex;
 }
@@ -31,29 +34,75 @@ contract SpoilerConditionalTokensV1 is ERC1155("url") {
   using SafeMath for uint8;
   using Address for address;
 
-  mapping(bytes32 => Condition) conditions;
-  
+  mapping(bytes32 => Condition) public conditions;
 
-  function prepareCondition(address oracle, bytes32 questionId, uint8 positionCount) external {
-    // condition 등록, 이미 존재하면 error
+  function prepareCondition(
+    IERC20 collateralToken, 
+    address oracle, 
+    bytes32 questionId, 
+    uint8 positionCount, 
+    uint256 startBlockNumber,
+    uint256 endBlockNumber
+    ) external {
+    // precondtion check
+    // condition 등록
+    Condition storage condition = conditions[getConditionId(oracle, questionId)];
+
+    condition.isInitialized = true;
+    condition.collateralToken = collateralToken;
+    condition.oracle = oracle;
+    condition.positionCount = positionCount;
+    condition.startBlockNumber = startBlockNumber;
+    condition.endBlockNumber = endBlockNumber;
+    condition.selectedIndex = type(uint256).max;
   }
 
   function resolve(bytes conditionId, uint8 selectedIdx) external {
+    Condition storage condition = conditions[conditionId];
+    require(condition.isInitialized == true, "SpoilerConditionalTokensV1: Not initialized");
+    require(condition.selectedIndex == type(uint256).max, "SpoilerConditionalTokensV1: Already resolved");
+    require(selectedIdx < condition.positionCount - 2, "SpoilerConditionalTokensV1: Not in range");
+
+    condition.selectedIndex = selectedIndex;
   }
 
-  function takePosition(bytes conditionId, uint8 positionIdx) external {
-    
+  function takePosition(bytes conditionId, uint8 positionIdx, uint256 amount) external {
+    // transfer token to here
+    // mint position token
+    // check if exceed the allowed position
+    Condition storage condition = conditions[conditionId];
+    require(condition.isInitialized == true, "SpoilerConditionalTokensV1: Not initialized");
+    uint256 positionId = getPositionId(conditionId, positionIdx);
+
+    IERC20(condition.collateralToken).transferFrom(msg.sender, address(this), amount);
+    _mint(msg.sender, positionId, amount, "");
+    condition.positionTotalSupply[positionId].add(amount);
   }
 
-  function redeem(bytes conditionId) external {
-    
+  function redeemPosition(bytes conditionId) external {
+    // redeem position by share;
+    Condition memory condition = conditions[conditionId];
+
+    uint256 winPositionId = getPositionId(condition.selectedIndex);
+    uint256 winPositonAmount = balanceOf(msg.sender, winPositionId);
+    uint256 loseTotalSupply = 0;
+    for (uint8 i = 0; i < condition.positionCount; i++) {
+      if (i == condition.selectedIndex) {
+        continue;
+      }
+      loseTotalSupply.add(condition.positionTotalSupply[getPositionId(conditionId, i)]);
+    }
+    uint256 prize = loseTotalSupply.mul(winPositonAmount).div(condition.positionTotalSupply[winPositionId]);
+
+    _burn(msg.sender, winPositionId, winPositonAmount);
+    IERC20(condition.collateralToken).transfer(address(this), prize.add(winPositonAmount));
   }
 
   function getConditionId(address oracle, bytes32 questionId) public pure returns(bytes32) {
-
+    return keccak256(abi.encodePacked(conditionId, positionIdx));
   }
 
-  function getPositionId() public pure returns(bytes32) {
-
+  function getPositionId(bytes conditionId, uint8 positionIdx) public pure returns(uint256) {
+    return uint256(keccak256(abi.encodePacked(conditionId, positionIdx)));
   }
 }
