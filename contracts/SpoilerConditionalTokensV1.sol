01bf9e2a577f52d448ed6c4dcbe3c59d9427446f
soobokjin <thdnthdn24@gmail.com>
2024-04-22 11:32:07 +0900

    Add restriction

--- contracts/SpoilerConditionalTokensV1.sol
+++ contracts/SpoilerConditionalTokensV1.sol
@@ -5,12 +5,9 @@ import {SafeMath} from "./utils/SafeMath.sol";
 import {Address} from "openzeppelin-contracts/contracts/utils/Address.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
-
+import {Ownable}  from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 // TODO: devide collateral treasury and this logic
-// TODO: owner
-// TODO: restriction
-// TODO: set eventlog
 
 // oracle, questionId 를 조합하여 condition Id 생성. (slot count 는 상관없음)
 // position Id: condition Id + position index 
@@ -28,24 +25,29 @@ struct Condition {
 }
 
 // TODO: setup url
-contract SpoilerConditionalTokensV1 is ERC1155("url") {
+contract SpoilerConditionalTokensV1 is Ownable, ERC1155 {
   using SafeMath for uint256;
   using SafeMath for uint8;
   using Address for address;
-
-  mapping(bytes32 => Condition) internal conditions;
-
-  // condition 생성
-  // take position eventlog <- user 별 포지션 정보 BE 에서 기록, 보여주기 위함
-  // user 별 총 금액, 승리 경험 등을 별도로 기록한다.
-  // user Address, fid, user betting 횟수, 승리, 진행중, 패배, 등을 기록하기 위함 (POINT)
-  // redeem 
 
   event PrepareCondition(bytes32 conditionId, bytes32 questionId, address oracle, uint positionCount);
   event ResolveCondition(bytes32 conditionId, address oracle, uint8 selectedIdx);
   event TakePosition(bytes32 indexed conditionId, address indexed positionBuyer, uint8 positionIdx, uint256 amount);
   event RedeemPosition(bytes32 indexed conditionId, address indexed positionBuyer, uint256 amount);
 
+  mapping(bytes32 => Condition) internal conditions;
+  uint256 maxPositionLimits;
+
+
+  constructor() {
+    ERC1155("url");
+
+    maxPositionLimits = type(uint256).max;
+  }
+
+  function setMaxPositionLimits(uint256 limits) Ownable() external {
+    maxPositionLimits = limits;  
+  }
 
   function prepareCondition(
     IERC20 collateralToken, 
@@ -54,12 +56,15 @@ contract SpoilerConditionalTokensV1 is ERC1155("url") {
     uint8 positionCount, 
     uint256 startBlockNumber,
     uint256 endBlockNumber
-    ) external {
+    ) Ownable() external {
     // precondtion check
-    // condition 등록
-    // position count 가 max - 1 보다 작아야 함
     bytes conditionId = getConditionId(oracle, questionId);
     Condition storage condition = conditions[conditionId];
+
+    require(condition.isInitialized, false, "SpoilerConditionalTokensV1: Already initialized");
+    require(positionCount < type(uint8).max, "SpoilerConditionalTokensV1: Exceed max position count");
+    require(startBlockNumber >= block.number, "SpoilerConditionalTokensV1: Invalid block number");
+    require(startBlockNumber < endBlockNumber, "SpoilerConditionalTokensV1: Invalid block number");
 
     condition.isInitialized = true;
     condition.collateralToken = address(collateralToken);
@@ -76,12 +81,13 @@ contract SpoilerConditionalTokensV1 is ERC1155("url") {
     return conditions[conditionId].isInitialized;
   }
 
-  function resolve(bytes32 conditionId, uint8 selectedIdx) external {
+  function resolve(bytes32 questionId, uint8 selectedIdx) external {
+    bytes32 conditionId = getConditionId(msg.sender, questionId);
     Condition storage condition = conditions[conditionId];
-    // TODO: only oracle can call this method
     require(condition.isInitialized == true, "SpoilerConditionalTokensV1: Not initialized");
+    require(block.number > condition.endBlockNumber, "SpoilerConditionalTokensV1: Condition not ended");
     require(condition.selectedIndex == type(uint8).max, "SpoilerConditionalTokensV1: Already resolved");
-    require(selectedIdx < condition.positionCount - 1, "SpoilerConditionalTokensV1: Not in range");
+    require(selectedIdx <= condition.positionCount - 1, "SpoilerConditionalTokensV1: Not in range");
 
     condition.selectedIndex = selectedIdx;
 
@@ -89,12 +95,11 @@ contract SpoilerConditionalTokensV1 is ERC1155("url") {
   }
 
   function takePosition(bytes32 conditionId, uint8 positionIdx, uint256 amount) external {
-    // transfer token to here
-    // mint position token
-    // check if exceed the allowed position
     Condition storage condition = conditions[conditionId];
-    require(condition.isInitialized == true, "SpoilerConditionalTokensV1: Not initialized");
     uint256 positionId = getPositionId(conditionId, positionIdx);
+    require(condition.isInitialized == true, "SpoilerConditionalTokensV1: Not initialized");
+    require(block.number >= condition.startBlockNumber && block.number <= condition.endBlockNumber, "SpoilerConditionalTokensV1: Condition not activated");
+    require(balanceOf(msg.sender, positionId).add(amount) < maxPositionLimits, "SpoilerConditionalTokensV1: Exceed max position");
 
     IERC20(condition.collateralToken).transferFrom(msg.sender, address(this), amount);
     _mint(msg.sender, positionId, amount, "");
@@ -106,6 +111,7 @@ contract SpoilerConditionalTokensV1 is ERC1155("url") {
   function redeemPosition(bytes32 conditionId) external {
     // redeem position by share;
     Condition storage condition = conditions[conditionId];
+    require(condition.selectedIndex != type(uint8).max, "SpoilerConditionalTokensV1: Not resolved");
 
     uint256 winPositionId = getPositionId(conditionId, condition.selectedIndex);
     uint256 winPositonAmount = balanceOf(msg.sender, winPositionId);
