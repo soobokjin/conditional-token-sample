61af821648ae25b86e52fbd3194be8cfc7e6a40a
soobokjin <thdnthdn24@gmail.com>
2024-04-28 16:47:03 +0900

    Add point logic to spoiler condtion token

--- contracts/SpoilerConditionalTokensV1.sol
+++ contracts/SpoilerConditionalTokensV1.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import {Address} from "openzeppelin-contracts/contracts/utils/Address.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable}  from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
@@ -28,8 +29,10 @@ struct Condition {
   uint8 selectedIndex;
 }
 
+
 // TODO: setup url
 contract SpoilerConditionalTokensV1 is ERC1155, Ownable {
+  using SafeERC20 for IERC20;
   using SafeMath for uint256;
   using SafeMath for uint8;
   using Address for address;
@@ -40,6 +43,7 @@ contract SpoilerConditionalTokensV1 is ERC1155, Ownable {
   event ResolveCondition(bytes32 conditionId, address oracle, uint8 selectedIdx);
   event TakePosition(bytes32 indexed conditionId, address indexed positionBuyer, uint8 positionIdx, uint256 amount);
   event RedeemPosition(bytes32 indexed conditionId, address indexed positionBuyer, uint256 amount);
+  event RedeemColleteral(address indexed pointOwner, address colleteralToken, uint256 amount);
 
   /// STATE VARIABLES ///
 
@@ -51,10 +55,12 @@ contract SpoilerConditionalTokensV1 is ERC1155, Ownable {
   
 
   constructor(address spoilerPoint) Ownable(msg.sender) ERC1155("url"){
+    // TODO: colleteral token and spoilerToken's backed token must be matched
     spoilerPoint = ISpoilerPoint(spoilerPoint);
     maxPositionLimits = type(uint256).max;
 
-    spoilerPoint.approve(address(this), type(uint256).max);
+    spoilerPoint.approve(address(spoilerPoint), type(uint256).max);
+    IERC20(spoilerPoint.backedToken()).approve(address(spoilerPoint), type(uint256).max);
   }
 
   /// VIEW FUNCTION ///
@@ -155,9 +161,6 @@ contract SpoilerConditionalTokensV1 is ERC1155, Ownable {
     emit ResolveCondition(conditionId, condition.oracle, selectedIdx);
   }
 
-  /**
-    TODO: mint point
-   */
   function takePosition(bytes32 conditionId, uint8 positionIdx, uint256 amount) external {
     Condition storage condition = conditions[conditionId];
     uint256 positionId = getPositionId(conditionId, positionIdx);
@@ -168,15 +171,13 @@ contract SpoilerConditionalTokensV1 is ERC1155, Ownable {
     require(positionAmount <= maxPositionLimits, "SpoilerConditionalTokensV1: Exceed max position");
 
     IERC20(condition.collateralToken).transferFrom(msg.sender, address(this), amount);
+    spoilerPoint.issuePointTo(address(this), amount);
     _mint(msg.sender, positionId, amount, "");
     condition.positionTotalSupply[positionId] = condition.positionTotalSupply[positionId].add(amount);
 
     emit TakePosition(conditionId, msg.sender, positionIdx, amount);
   }
 
-  /**
-    TODO: send minted point
-   */
   function redeemPosition(bytes32 conditionId) external {
     // redeem position by share;
     Condition storage condition = conditions[conditionId];
@@ -195,17 +196,17 @@ contract SpoilerConditionalTokensV1 is ERC1155, Ownable {
     uint256 redeemAmount = prize.add(winPositonAmount);
 
     _burn(msg.sender, winPositionId, winPositonAmount);
-    IERC20(condition.collateralToken).transfer(msg.sender, prize.add(winPositonAmount));
+    spoilerPoint.transfer(msg.sender, redeemAmount);
 
     emit RedeemPosition(conditionId, msg.sender, redeemAmount);
   }
 
-  /**
-    TODO: redeem point to colleteral
-   */
-  // 담보물은 단일로 가져가는 것이 좋을 것 같음. 아니면 담보물 별 ERC20 을 보유하고 있어야 함 (이에따라 담보물 토큰도 condition 별로 변경할 수 없도록 할 필요가 있음)
   function redeemColleteral(uint256 pointAmount) external {
+    spoilerPoint.transferFrom(msg.sender, address(this), pointAmount);
+    spoilerPoint.burnPointFrom(address(this), pointAmount);
+    IERC20(spoilerPoint.backedToken()).safeTransfer(msg.sender, pointAmount);
 
+    emit RedeemColleteral(msg.sender, address(spoilerPoint.backedToken()), pointAmount);
   }
 
 }
